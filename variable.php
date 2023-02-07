cf9f72ecd13420000657f19e5a5dee69d1e47b8c
bishalshrestha <bishal22001@student.hamk.fi>
2023-02-07 10:54:58 +0200

    Initial commit

---
+++ variable.php
@@ -0,0 +1,11 @@
+<?php include "header.php" ?>
+<form action="action.php" method="post">
+
+Firstname: <input type="text" name="fname"> <br>
+Lastname: <input type="text" name="lname"> <br>
+Birthdate: <input type="date" name="bdate"> <br>
+<input type="submit" value="submit">
+
+</form>
+
+<?php include "footer.php" ?>
